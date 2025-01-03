575c813ab7f50909998670c6c755a763e4e7e07d
Chad Eckles <chad1020@gmail.com>
2025-01-03 13:03:09 -0600

    base rest api

---
+++ src/main/java/com/mt/rps/settlingscore/ScoreController.java
@@ -0,0 +1,121 @@
+package com.mt.rps.settlingscore;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+public class ScoreController {
+    static Score score = new Score(30, 20, 10);
+
+    @GetMapping("/health-check")
+    public String getHealthCheck() {
+        return "Situation Normal All Fired Up";
+    }
+
+    @GetMapping("/score")
+    public Score getScore() {
+        return score;
+    }
+
+    // Basic way create each endpoint
+    //    @GetMapping("/score/wins")
+    //    public int getWins() {
+    //        return score.getWins();
+    //    }
+    //
+    //    @GetMapping("/score/ties")
+    //    public int getTies() {
+    //        return score.getTies();
+    //    }
+    //
+    //    @GetMapping("/score/losses")
+    //    public int getLosses() {
+    //        return score.getLosses();
+    //    }
+
+    //Better way use path variable.
+    @GetMapping("/score/{winlossesties}")
+    public int getWinLossesOrTies(@PathVariable String winlossesties) {
+        if (winlossesties.equalsIgnoreCase("wins")) {
+            return score.getWins();
+        } else if (winlossesties.equalsIgnoreCase("losses")) {
+            return score.getLosses();
+        } else {
+            return score.getTies();
+        }
+    }
+
+    //Increase Wins
+    //Individual route
+//    @PostMapping("/score/wins")
+//    public Score increaseWins(){
+//        score.setWins(score.getWins() + 1);
+//        return score;
+//    }
+//    @PostMapping("/score/losses")
+//    public Score increaseLosses(){
+//        score.setLosses(score.getLosses() + 1);
+//        return score;
+//    }
+//    @PostMapping("/score/ties")
+//    public Score increaseTies(){
+//        score.setTies(score.getTies() + 1);
+//        return score;
+//    }
+
+    //Increase the socre values
+    //Streamline route
+    @PostMapping("/score/{winslossesties}")
+    public Score setWinLossesOrTies(@PathVariable String winslossesties) {
+        if (winslossesties.equalsIgnoreCase("wins")) {
+            score.setWins(score.getWins() + 1);
+            return score;
+        } else if (winslossesties.equalsIgnoreCase("losses")) {
+            score.setLosses(score.getLosses() + 1);
+            return score;
+        } else {
+            score.setTies(score.getTies() + 1);
+            return score;
+        }
+    }
+// Individual routes
+//    @PatchMapping("/score/wins")
+//    public Score updateWins(@RequestParam(name="new-value")int newValue){
+//        score.setWins(newValue);
+//        return score;
+//    }
+//    @PatchMapping("/score/losses")
+//    public Score updateLosses(@RequestParam(name="new-value")int newValue){
+//        score.setLosses(newValue);
+//        return score;
+//    }
+//    @PatchMapping("/score/ties")
+//    public Score updateTies(@RequestParam(name="new-value")int newValue){
+//        score.setTies(newValue);
+//        return score;
+//    }
+
+    //Group Routes
+    //Patch Route for score to patch a score
+    @PatchMapping("/score/{winslossesties}")
+    public Score patchWinLossesOrTies(@PathVariable String winslossesties, @RequestParam(name = "new-value") int newValue) {
+        if (winslossesties.equalsIgnoreCase("wins")) {
+            score.setWins(newValue);
+            return score;
+        } else if (winslossesties.equalsIgnoreCase("losses")) {
+            score.setLosses(newValue);
+            return score;
+        } else {
+            score.setTies(newValue);
+            return score;
+        }
+    }
+
+
+    //Put Route for score to replace score
+    @PutMapping("/score")
+    public Score replaceScore( @RequestBody Score newScore) {
+        score = newScore;
+        return score;
+    }
+}
---
+++ src/main/java/com/mt/rps/settlingscore/SettlingScoreApplication.java
@@ -0,0 +1,13 @@
+package com.mt.rps.settlingscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlingScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettlingScoreApplication.class, args);
+	}
+
+}
